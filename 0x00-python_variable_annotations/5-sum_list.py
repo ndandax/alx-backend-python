bfa6b36ce72c8e977d215bb3e83263097aa8788c
ndandax <sebandandala99@gmail.com>
2023-10-05 16:03:38 +0300

    annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a module to define a function to sum to floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of lists"""
+    return sum(input_list)
