c5b5e18fb93ab98a917953fec626115e57403811
ndandax <sebandandala99@gmail.com>
2023-10-05 15:37:18 +0300

    annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a module to return string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """returns string representation of a function"""
+    return str(n)
