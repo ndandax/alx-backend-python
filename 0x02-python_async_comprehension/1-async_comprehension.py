f760bc539c42221c1629724f492c7fb663e9c9ce
ndandax <sebandandala99@gmail.com>
2023-10-10 16:07:39 +0300

    async

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""coroutine called async_generator that takes no arguments. """
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async comprehension"""
+    result = [i async for i in async_generator()]
+    return result
