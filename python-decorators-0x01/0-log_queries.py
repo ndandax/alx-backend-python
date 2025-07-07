84438be057bdda1fa72d389dc9244085e841a7b3
ndandax <sebandandala99@gmail.com>
2025-07-07 05:07:26 -0400

    alx

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,33 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+
+def log_queries(func):
+   @functools.wraps(func)
+   def wrapper(*args, **kwargs):
+      args_repr = [repr(a) for a in args]
+      kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
+      ##print(args_repr)
+      print(kwargs_repr)
+      func(*args, **kwargs)
+      return func(*args, **kwargs)
+   
+   return wrapper
+      
+      
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+
