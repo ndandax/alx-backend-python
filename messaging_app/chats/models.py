45d090cee5904613f924869e054e88dcc79de9c6
ndandax <sebandandala99@gmail.com>
2025-07-22 15:05:01 -0400

    alx7

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,32 @@
-import uuid
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+import uuid
 from django.utils import timezone
 
-class User(AbstractUser):
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, role, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            role=role,
+            **extra_fields
+        )
+        user.set_password(password)  # hashes password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, role='admin', password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, first_name, last_name, role, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
         ('guest', 'Guest'),
         ('host', 'Host'),
@@ -11,16 +34,24 @@ class User(AbstractUser):
     )
 
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=128, db_column='password_hash')  # maps to password_hash in DB
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     created_at = models.DateTimeField(default=timezone.now)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+        return self.email
 
 
 class Conversation(models.Model):
