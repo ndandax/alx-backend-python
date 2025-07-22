737d2e66f88b1e7d34d3b59394def6fed7e6241d
ndandax <sebandandala99@gmail.com>
2025-07-22 15:38:06 -0400

    alx

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from rest_framework import filters
 from rest_framework.decorators import action
 from .models import Conversation, Message
 from .serializers import (
@@ -14,6 +15,9 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
+    search_fields = ['participants__email', 'participants__first_name']
+    ordering_fields = ['created_at']
+
     def get_serializer_class(self):
         if self.action == 'create':
             return ConversationCreateSerializer
@@ -21,19 +25,26 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         conversation = serializer.save()
-        # You can attach the current user automatically if needed
-        # conversation.participants.add(self.request.user)
-        # conversation.save()
+        conversation.participants.add(self.request.user)
+        conversation.save()
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['message_body']  
+    ordering_fields = ['sent_at']   
+
     def get_serializer_class(self):
         if self.action == 'create':
             return MessageCreateSerializer
         return MessageSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+
+    def perform_create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
