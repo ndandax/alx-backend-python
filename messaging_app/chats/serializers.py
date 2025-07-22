c37bd8436cf210423b9fa494ffcc586d33c94302
ndandax <sebandandala99@gmail.com>
2025-07-22 15:11:11 -0400

    alx9

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
-
+from rest_framework.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.CharField()
+    role = serializers.CharField()
+
+    def validate_role(self, value):
+        allowed_roles = ['guest', 'host', 'admin']
+        if value not in allowed_roles:
+            raise ValidationError("Role must be one of: guest, host, admin")
+        return value
+
     class Meta:
         model = User
         fields = [
@@ -17,14 +28,19 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ['user_id', 'created_at']
 
 
+
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)  # show full sender details
+    sender_full_name = serializers.SerializerMethodField()
+
+    def get_sender_full_name(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
 
     class Meta:
         model = Message
         fields = [
             'message_id',
             'sender',
+            'sender_full_name',
             'message_body',
             'sent_at'
         ]
